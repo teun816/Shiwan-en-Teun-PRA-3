e1b2fc3f7a6d5b6d4be380350a82d381eb593e68
teun816 <teunvandenouden106@gmail.com>
2026-03-11 14:05:59 +0100

    1.3

--- backend/tasksController.php
+++ backend/tasksController.php
@@ -1,24 +1,51 @@
 <?php
+$action = $_POST['action'];
 
-$titel = $_POST['titel'];
-$beschrijving = $_POST['beschrijving'];
-$afdeling = $_POST['afdeling'];
+if($action == "create"){
+    $titel = $_POST['titel'];
+    $beschrijving = $_POST['beschrijving'];
+    $afdeling = $_POST['afdeling'];
 
-echo $attractie . " / " . $capaciteit . " / " . $melder;
+    echo $attractie . " / " . $capaciteit . " / " . $melder;
 
-require_once 'config.php';
+    require_once 'config.php';
 
-$query = "INSERT INTO taken (titel, beschrijving, afdeling)
-VALUES(:titel, :beschrijving, :afdeling)";
+    $query = "INSERT INTO taken (titel, beschrijving, afdeling)
+    VALUES(:titel, :beschrijving, :afdeling)";
 
-$statement = $conn->prepare($query);
+    $statement = $conn->prepare($query);
 
-$statement->execute([
-    ":titel" => $titel,
-    ":beschrijving" => $beschrijving,
-    ":afdeling" => $afdeling,
-]);
+    $statement->execute([
+        ":titel" => $titel,
+        ":beschrijving" => $beschrijving,
+        ":afdeling" => $afdeling,
+    ]);
 
-$items = $statement->fetchAll(PDO::FETCH_ASSOC);
+    $items = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-header("Location: ../Task/index.php?msg=Taak opgeslage");
+    header("Location: ../Task/index.php?msg=Taak opgeslage");
+}
+
+if($action == "update"){
+    $id = $_POST['id'];
+    $titel = $_POST['titel'];
+    $beschrijving = $_POST['beschrijving'];
+    $afdeling = $_POST['afdeling'];
+
+    require_once 'conn.php';
+    $query = "UPDATE meldingen 
+    SET titel = :titel, beschrijving = :beschrijving, afdeling = :afdeling
+    WHERE id = :id";
+    $statement = $conn->prepare($query);
+    $statement->execute([
+        ":titel" => $titel,
+        ":beschrijving" => $beschrijving,
+        ":afdeling" => $afdeling,
+        "id" => $id
+    ]);
+    $melding = $statement->fetch(PDO::FETCH_ASSOC);
+    header("Location: ../index.php?msg=Taak opgeslagen");
+}
+
+if($action == "delete"){
+}
---
+++ tasks/edit.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="nl">
+
+<head>
+    <title>Taken / Aanpassen</title>
+    <?php require_once __DIR__.'../head.php'; ?>
+</head>
+
+<body>
+    <?php 
+
+    if(!isset($_GET['id'])){
+        echo "Geef in je aanpaslink op de index.php het id van betreffende item mee achter de URL in je a-element om deze pagina werkend te krijgen na invoer van je vijfstappenplan";
+        exit;
+
+    }
+    ?>
+    <?php
+        require_once __DIR__.'../head.php'; ?>
+
+    <div class="container">
+        <h1>Taken aanpassen</h1>
+
+        <?php
+        $id = $_GET['id'];
+
+        require_once '../backend/conn.php';
+
+        $query = "SELECT * FROM taken WHERE id = :id";
+
+        $statement = $conn->prepare($query);
+
+        $statement->execute([":id" => $id]);
+
+        $taken = $statement->fetch(PDO::FETCH_ASSOC);
+        ?>
+
+        <form action="<?php echo $base_url; ?>../backend/tasksController.php" method="POST">
+            <input type="hidden" name="action" value="update">
+            <input type="hidden" name="id" value="<?php echo $melding['id']; ?>">
+
+            <label for="titel">Titel</label>
+            <input type="text" id="titel" name="titel" value="<?php echo $taken['titel']; ?>">
+
+            <label for="beschrijving">Beschrijving</label>
+            <input type="text" id="beschrijving" name="beschrijving" value="<?php echo $taken['beschrijving']; ?>">
+
+            <label for="afdeling">Afdeling</label>
+            <input type="text" id="afdeling" name="afdeling" value="<?php echo $taken['afdeling']; ?>">
+            
+            <input type="submit" value="Melding opslaan">
+
+        </form>
+    </div>  
+
+</body>
+
+</html>
