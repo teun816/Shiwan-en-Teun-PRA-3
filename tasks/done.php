4f6475316029fa14d16b7323680529965f6685b8
Shiwan2007 <D311583@edu.curio.nl>
2026-04-08 13:47:57 +0200

    done

--- tasks/done.php
+++ tasks/done.php
@@ -10,8 +10,7 @@ if (!isset($_SESSION['user_id']))
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php require_once '../head.php'; ?>
     <title>Document</title>
 </head>
 
