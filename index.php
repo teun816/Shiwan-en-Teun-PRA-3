be28ccbe69981d933a02d8d074339244ba1a7daa
Shiwan2007 <D311583@edu.curio.nl>
2026-04-09 15:57:55 +0200

    style.css

--- index.php
+++ index.php
@@ -24,6 +24,7 @@ if (!isset($_SESSION['user_id']))
     <div class="board">
 
         <div class="column">
+            <div class="teDoen">
             <h2>Te doen</h2>
             <?php foreach ($taken as $taak): ?>
             <?php if ($taak['status'] == 'Te doen'): ?>
@@ -32,9 +33,11 @@ if (!isset($_SESSION['user_id']))
                 </div>
             <?php endif; ?>
             <?php endforeach; ?>
+            </div>
         </div>
 
         <div class="column">
+            <div class="bezig">
             <h2>Bezig</h2>
             <?php foreach ($taken as $taak): ?>
             <?php if ($taak['status'] == 'Bezig'): ?>
@@ -43,9 +46,11 @@ if (!isset($_SESSION['user_id']))
                 </div>
             <?php endif; ?>
             <?php endforeach; ?>
+            </div>
         </div>
 
         <div class="column">
+          <div class="klaar">
             <h2>Done</h2>
             <?php foreach ($taken as $taak): ?>
             <?php if ($taak['status'] == 'Klaar'): ?>
@@ -54,6 +59,7 @@ if (!isset($_SESSION['user_id']))
                 </div>
             <?php endif; ?>
             <?php endforeach; ?>
+         </div>
         </div>
 
     </div>
